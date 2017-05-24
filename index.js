c3f47cfe25ee2c5110a4f6acbe3081c7f289b72d
Brice Culas <culas.brice@gmail.com>
2017-05-24 12:09:25 +0200

    improvement

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
  * Created by admin on 27/02/2017.
  */
 //var socket = require('socket.io-client')('http://192.168.1.191:3000/',{ query: 'role=sensor&sensorId=1'});
-//var socket = require('socket.io-client')('http://52.59.94.92:3000/',{ query: 'role=sensor&sensorId=2'});
+//var socket = require('socket.io-client')('http://52.59.94.92:3000/',{ query: 'role=sensor&sensorId=42'});
+//var socket = require('socket.io-client')('http://52.58.113.84:3000/',{ query: 'role=sensor&sensorId=42'});
 var socket = require('socket.io-client')('http://localhost:3000/',{ query: 'role=sensor&sensorId=1'});
 //var socket = require('socket.io-client')('http://35.158.33.67:3000/',{ query: 'farmId=farm1'});
 var SocketActions = require('./SocketActions');
@@ -39,91 +40,91 @@ socket.on('connect', function(){
             'sensor_id': '1',
             'sensor_value': getRandomInt(0,100)
         };
-        //console.log(_data);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[0],
             'sensor_id': '1',
-            'sensor_value': getRandomInt(0,100)
+            'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[1],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[2],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[3],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[4],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[5],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[7],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[8],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[9],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[10],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[11],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[12],
             'sensor_id': '1',
             'sensor_value': value
         }));
-        value = getRandomInt(0,100);
+        value = getRandomInt(0,100).toString();
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': types[13],
@@ -134,30 +135,30 @@ socket.on('connect', function(){
             'device_id': '1',
             'sensor_type': 'relay1',
             'sensor_id': '1',
-            'sensor_value': 0
+            'sensor_value': '0'
         }));
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': 'relay2',
             'sensor_id': '1',
-            'sensor_value': 1
+            'sensor_value': "1"
         }));
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': 'relay3',
             'sensor_id': '1',
-            'sensor_value': 0
+            'sensor_value': "0"
         }));
         socket.emit(SocketActions.SENSOR_EMIT,JSON.stringify({
             'device_id': '1',
             'sensor_type': 'relay4',
             'sensor_id': '1',
-            'sensor_value': 1
+            'sensor_value': "1"
         }));
         console.log(i);
         i = i+1;
         if (i<500000)
-            setTimeout(sendData,1000)
+            setTimeout(sendData,500)
     }
     setTimeout(sendData,1);
 });
